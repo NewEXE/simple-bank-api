8b040d665126749ceaf4061ce529587e4f8b698b
Vlad Voloshyn <v.voloshyn@codetiburon.com>
2018-06-11 17:18:05 +0300

    Added password reset routes; Removed views: welcome, auth/register

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,18 @@
 |
 */
 
+// Authentication Routes
 Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Password Reset Routes
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+//Auth zone routes
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
